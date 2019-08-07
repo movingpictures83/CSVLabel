17f4a60203da3d7aa2a708eede2b169125a1c928
Trevor Cickovski <tcickovs@castalia.cs.fiu.edu>
2019-08-07 16:10:30 -0400

    Initial commit

---
+++ CSVLabelPlugin.py
@@ -0,0 +1,23 @@
+class CSVLabelPlugin:
+   def input(self, filename):
+      f = open(filename, 'r')
+      self.params = dict()
+      for line in f:
+         stuff = line.split('\t')
+         self.params[stuff[0]] = stuff[1].strip()
+
+
+   def run(self):
+      g = open(self.params['inputfile'], 'r')
+      self.contents = ""
+      pos = 0
+      for line in g:
+         if (pos == 0):
+            line = "\"\"," + line
+         else: 
+            line = self.params['label'] + "," + line
+         self.contents += line
+
+   def output(self, filename):
+      g = open(filename, 'w')
+      g.write(self.contents)
